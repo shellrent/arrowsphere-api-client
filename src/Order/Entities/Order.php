76b6bc8f50c24d5f989f5dfe594ac8ac1b998075
Armando Caprio <armando@shellrent.com>
2022-02-24 11:57:39 +0100

    Cambiato nome pacchetto

---
+++ src/Order/Entities/Order.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace ArrowSphere\PublicApiClient\Order\Entities;
+
+use ArrowSphere\PublicApiClient\AbstractEntity;
+
+class Order extends AbstractEntity {
+	
+	
+	
+	public function jsonSerialize() {
+	
+	}
+	
+}
